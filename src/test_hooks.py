eef5e2f4010a1c2beb15cde43a8ba1b7299d899b
Pierre Grothé <pierre@grothe.ca>
2025-09-05 07:44:54 -0400

    chore: remove test file

--- src/test_hooks.py
+++
@@ -1,9 +0,0 @@
-"""Test file to verify pre-commit hooks run automatically."""
-
-
-def test_function():
-    x = 1 + 2  # Formatting issue: missing spaces around operators
-    return x
-
-
-print("Should use double quotes")  # Style issue: single quotes
